c421fd21640eb127279fa982c93d38946f5e3378
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-01-09 11:46:24 +0100

    Remove switch

--- Click/ViewController.swift
+++ Click/ViewController.swift
@@ -12,16 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     var toggle = false
-    var toggleSwitch = false
 
     @IBOutlet weak var ButtonClick: UIButton!
+    @IBOutlet weak var Background: UIView!
+    
     @IBAction func ButtonClickAction(_ sender: UIButton) {
         if(!toggle){
-            ButtonClick.setTitle("Dziękuję serdecznie!", for: .normal)
+            ButtonClick.setTitle("Dziękuję serdecznie :)", for: .normal)
             toggle = true;
         }
         else{
@@ -33,15 +33,7 @@ class ViewController: UIViewController {
     @IBAction func Rotate(_ sender: Any) {
         ButtonClick.setTitle("Co ty wyrabiasz?!", for: .normal)
     }
-    @IBOutlet weak var Background: UIView!
-    @IBAction func Switcher(_ sender: UISwitch) {
-        if (!toggleSwitch){
-            Background.backgroundColor = UIColor(red: 0.60, green: 0.00, blue: 1.00, alpha: 1.00)
-            toggleSwitch = true;
-        }else{
-            Background.backgroundColor = UIColor(red: 0.50, green: 0.00, blue: 1.00, alpha: 1.00)
-            toggleSwitch = false;
-        }
-    }
+    
+    
 }
 
